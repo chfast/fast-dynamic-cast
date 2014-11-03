3a2790276ae036db76014f75d3cc9f97402aaa15
Paweł Bylica <chfast@gmail.com>
2014-11-04 00:05:03 +0100

    child_order and child_index helpers

--- typelists.cpp
+++ typelists.cpp
@@ -14,6 +14,7 @@
 #include <boost/mpl/find_if.hpp>
 #include <boost/mpl/contains.hpp>
 #include <boost/mpl/if.hpp>
+#include <boost/mpl/order.hpp>
 
 using namespace boost;
 using namespace mpl;
@@ -72,6 +73,19 @@ template<typename Types, typename T> struct level {
 	static const auto value = level_from_base<Types, T, _is_root>::value;
 };
 
+template<typename Types, typename T> struct child_order {
+	using _base = typename ::base<Types, T>::type;
+	using _bros = typename dsubs_set<Types, _base>::type;
+	static const auto value = order<_bros, T>::value;
+};
+
+template<typename Types, typename T> struct child_index {
+	using _base = typename ::base<Types, T>::type;
+	using _bros = typename dsubs<Types, _base>::type;
+	using _it = typename find<_bros, T>::type;
+	static const auto value = _it::pos::value;
+};
+
 
 
 
@@ -79,9 +93,13 @@ class A { using a = A; };
 
 class B: public A { using b = B; };
 
+class B2: public A {};
+
 class C: public B { using c = C;  };
 
-using types = mpl::list<A, B, C>;
+class C2: public B {};
+
+using types = mpl::list<A, B, C, B2, C2>;
 
 using a_bases = bases<types, A>::type;
 
@@ -95,22 +113,22 @@ static_assert(size<Roots>::value == 1, "");
 static_assert(is_same<front<Roots>::type, A>::value, "");
 
 using a_subs = subs<types, A>::type;
-static_assert(size<a_subs>::value == 2, "");
+static_assert(size<a_subs>::value == 4, "");
 
 using b_subs = subs<types, B>::type;
-static_assert(size<b_subs>::value == 1, "");
+static_assert(size<b_subs>::value == 2, "");
 
 using c_subs = subs<types, C>::type;
 static_assert(size<c_subs>::value == 0, "");
 
 using a_dsubs = dsubs<types, A>::type;
-static_assert(size<a_dsubs>::value == 1, "");
+static_assert(size<a_dsubs>::value == 2, "");
 
 using b_dsubs = dsubs<types, B>::type;
-static_assert(size<b_dsubs>::value == 1, "");
+static_assert(size<b_dsubs>::value == 2, "");
 
 using b_dsubs_set = dsubs_set<types, B>::type;
-static_assert(size<b_dsubs_set>::value == 1, "");
+static_assert(size<b_dsubs_set>::value == 2, "");
 
 using c_base = ::base<types, C>::type;
 
@@ -125,3 +143,8 @@ static_assert(level<types, B>::value == 1, "");
 
 static_assert(level<types, C>::value == 2, "");
 
+static_assert(child_order<types, B>::value == 2, "");
+static_assert(child_index<types, B>::value == 0, "");
+static_assert(child_index<types, B2>::value == 1, "");
+static_assert(child_order<types, C>::value == 2, "");
+static_assert(child_order<types, C2>::value == 3, "");
